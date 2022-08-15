fdfa15f7ff9cfd76cc651e6f5a13514c049fb664
Maulana Akbar Yudistika <yudistika.akbar@gmail.com>
2022-08-15 12:10:33 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div>
-      <h1>Test!</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <h1>Navbar</h1>
+        <span>
+          <h1>Page 1</h1>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
